320d9a9642ac32d08805b9bb04e6b3d40601dfce
laomatt & Stephanie Lopez <pair+laomatt.snlsf@devbootcamp.com>
2015-04-17 15:38:06 -0700

    feature business dash

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -2,4 +2,16 @@ class DealsController < ApplicationController
   def index
     @deals = Business.find(params[:business_id]).deals
   end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def destroy
+
+  end
 end
