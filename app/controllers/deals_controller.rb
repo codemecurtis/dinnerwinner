59ecfd389ce41443eaeb6866d739af99806706bd
laomatt & Stephanie Lopez <pair+laomatt.snlsf@devbootcamp.com>
2015-04-17 15:26:52 -0700

    create the index action for the deals controller

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -1,2 +1,5 @@
 class DealsController < ApplicationController
+  def index
+    @deals = Business.find(params[:business_id]).deals
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   devise_for :businesses
+  resources :businesses do
+    resources :deals
+  end
   devise_for :customers
 
-  resources :deals
+
+
 
 
 
