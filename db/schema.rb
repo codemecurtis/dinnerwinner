f0fd208fc1b8da48678e48a8afb930623d14f3fe
laomatt & Stephanie Lopez <pair+laomatt.snlsf@devbootcamp.com>
2015-04-17 14:59:20 -0700

    modified deals migration and created seed data

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150417185434) do
+ActiveRecord::Schema.define(version: 20150417200735) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,21 +74,8 @@ ActiveRecord::Schema.define(version: 20150417185434) do
     t.string   "short_description"
     t.string   "deal_image"
     t.integer  "business_id"
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.inet     "current_sign_in_ip"
-    t.inet     "last_sign_in_ip"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
   end
-
-  add_index "deals", ["email"], name: "index_deals_on_email", unique: true, using: :btree
-  add_index "deals", ["reset_password_token"], name: "index_deals_on_reset_password_token", unique: true, using: :btree
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+require 'faker'
+
+include Faker
+50.times do
+  Customer.create(first_name:Faker::Name.first_name, last_name:Faker::Name.last_name, password:'password', phone_number:Faker::PhoneNumber, email:Faker::Internet.email, avatar:Avatar.image)
+end
+
+30.times do
+  temp = Business.create(name:Faker::Company.name, phone_number:Faker::PhoneNumber, location:"#{Faker::Address.street_address}, #{Faker::Address.city}, #{Faker::Address.state} #{Faker::Address.zip}", logo:Faker::Company.logo, email:Faker::Internet.email)
+
+  number = Random.rand(11)
+  number.times do
+    temp.deals << Deal.create(name:Faker::Company.catch_phrase, short_description:Faker::Company.bs, deal_image:Faker::Company.logo)
+  end
+
+end
+
