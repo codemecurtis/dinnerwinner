006080d41a91c3e88a75c41e7d5f0709a059bb5e
laomatt & Stephanie Lopez <pair+laomatt.snlsf@devbootcamp.com>
2015-04-18 16:13:33 -0700

    deals crud working, files effected: deals.js deals_controller.rb and deals index.html

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -5,20 +5,43 @@ class DealsController < ApplicationController
   end
 
   def show
-    @deals = Business.find(params[:business_id]).deals.order(:created_at).reverse()
-    render :json => @deals
+    @deals = Business.find(params[:business_id]).deals.order(created_at: :asc).reverse()
+    p 't'*90
+    newarray=[]
+    @deals.each do |h|
+      name = h.name
+      sd = h.short_description
+      pic = h.deal_image
+      id = h.id
+      created_at = h.created_at.strftime('%A, %d %b %Y %l:%M %p')
+      newarray << {id: id, name: name, short_description: sd, deal_image: pic, created_at: created_at}
+    end
+    p newarray
+    render :json => newarray
   end
 
   def create
     @business = Business.find(params[:business_id])
-    @business.deals << Deal.create(name: params[:title], short_description: params[:content], deal_image: params[:image])
-    render :json => @business.deals
+    newdeal = Deal.create(name: params[:title], short_description: params[:content], deal_image: params[:image])
+    @business.deals << newdeal
+    @deals = Business.find(params[:business_id]).deals.order(created_at: :asc).reverse()
+    newarray=[]
+    @deals.each do |h|
+      name = h.name
+      sd = h.short_description
+      pic = h.deal_image
+      id = h.id
+      created_at = h.created_at.strftime('%A, %d %b %Y %l:%M %p')
+      newarray << {id: id, name: name, short_description: sd, deal_image: pic, created_at: created_at}
+    end
+    p newarray
+    render :json => newarray
   end
 
   def update
     @deal = Deal.find(params[:id])
-    p "#{@deal.name} --------------------"
     @deal.update_attributes(name: params[:title], short_description: params[:edit_content], deal_image: params[:image])
+
     render :json => @deal
   end
 
