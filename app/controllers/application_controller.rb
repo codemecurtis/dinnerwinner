b78a6eefd1f7b245c24dc37893beeb1957be1ea6
infin80design & Iulia-Soimaru <pair+infin80design.iulia-soimaru@devbootcamp.com>
2015-04-17 19:07:37 -0700

    redirect user to login form if user is not logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  def authenticate
+    redirect_to(new_customer_session_path) if current_customer.nil?
+  end
+
   def after_sign_in_path_for(customer)
     "/customers/#{customer.id}"
   end
