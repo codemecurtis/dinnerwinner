9501c01b3ced15ed40a466146ae979722e6cb36f
Curtis Seaton <infin80design@gmail.com>
2015-04-18 18:56:51 -0700

    Filter mass deals on business dashboard

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -1,10 +1,20 @@
 class BusinessesController < ApplicationController
+   skip_before_action :verify_authenticity_token, only: :create_mass_deal
   include BusinessesHelper
   def index
   end
 
   def show
     viewed_business
+    business_deals = Deal.where(business_id: params[:id])
+    @mass_deal = []
+    business_deals.each do |deal|
+      CustomerDeal.where(deal_id: deal.id).each do |deals|
+        if deals.mass_deal
+          @mass_deal << deals
+        end
+      end
+    end
     authenticate_business
   end
 
@@ -13,10 +23,12 @@ class BusinessesController < ApplicationController
   end
 
   def create_mass_deal
-    # CustomerDeal.new(business_id:, customer_id:)
+    CustomerDeal.create(deal_id:params[:deal], party_size: params[:party_size], mass_deal: true)
+    redirect_to action: 'show', id: current_business.id
   end
 
   def mass_deal
+    @deals = Deal.where(business_id: params[:business_id])
   end
 
 end
