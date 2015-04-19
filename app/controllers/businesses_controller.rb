84e62394fada2d15cefe6978cc2efe7b5c49a0d8
laomatt <laomatt1@gmail.com>
2015-04-19 11:57:34 -0700

    initial try of our display logic

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -20,35 +20,37 @@ class BusinessesController < ApplicationController
     @array_of_requests=[]
     CustomerDeal.all.each do |deal|
       temp = Deal.find(deal.deal_id)
-      if Business.find(params[:id]).neighborhood == deal.neighborhood
-    # if params[:id]==temp.business_id
-    @array_of_requests << deal
-  end
-end
-
-
-@accepted_deals = []
-business_deals.each do |deal|
-  CustomerDeal.where(deal_id: deal.id).each do |deals|
-    if deals.accepted
-      @accepted_deals << deals
+      bneigh = JSON.parse(Business.find(params[:id]).neighborhoods)
+      dneigh = deal.neighborhoods
+      if bneigh.any? {|e| dneigh.include?(e)}
+        @array_of_requests << deal
+      end
     end
+
+
+    @accepted_deals = []
+    business_deals.each do |deal|
+      CustomerDeal.where(deal_id: deal.id).each do |deals|
+        if deals.accepted
+          @accepted_deals << deals
+        end
+      end
+    end
+    authenticate_business
   end
-end
-authenticate_business
-end
 
-def edit
-  viewed_business
-end
 
-def create_mass_deal
-  CustomerDeal.create(deal_id:params[:deal], party_size: params[:party_size], mass_deal: true)
-  redirect_to action: 'show', id: current_business.id
-end
+  def edit
+    viewed_business
+  end
 
-def mass_deal
-  @deals = Deal.where(business_id: params[:business_id])
-end
+  def create_mass_deal
+    CustomerDeal.create(deal_id:params[:deal], party_size: params[:party_size], mass_deal: true)
+    redirect_to action: 'show', id: current_business.id
+  end
+
+  def mass_deal
+    @deals = Deal.where(business_id: params[:business_id])
+  end
 
 end
