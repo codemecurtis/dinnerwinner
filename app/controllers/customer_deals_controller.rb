af448df8c8a876f08177d86e6a5da58eeac63a7c
Stephanie Lopez & Iulia-Soimaru <pair+SNLSF.iulia-soimaru@devbootcamp.com>
2015-04-18 21:32:29 -0700

    create neighbohoods column in customer_deals table

--- app/controllers/customer_deals_controller.rb
+++ app/controllers/customer_deals_controller.rb
@@ -4,10 +4,11 @@ class CustomerDealsController < ApplicationController
   end
 
   def create
-    p "*" * 100
-    p params
     @new_customer_deal = CustomerDeal.create(customer_id: current_customer.id, party_size: params[:party_size], reservation_time: params[:reservation_time])
+    # params[:neighborhoods]
     render json: {customer: @new_customer_deal}, status: :created
   end
 
 end
+# business.neighborhoods
+# [alamo square, haight]
