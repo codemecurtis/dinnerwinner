8722d4e8a46a8226e8d5d87f559825728f71ba27
SNLSF <stephanielopez.sf@gmail.com>
2015-04-20 11:54:40 -0700

    fix ajax errors in customer dashboard by moving ajax call to business dashboard

--- app/assets/javascripts/businesses.js
+++ app/assets/javascripts/businesses.js
@@ -1,26 +1,5 @@
 $(document).ready(function(){
 
-  $(document).on('page:change', function(){
-// $(document).on('page:load', function(){
-  $.ajax({
-    url: window.location+'/request_list',
-  }).done(function(data){
-    var context =  { requests: data};
-    var source = $('#requests-template').html();
-    var template = Handlebars.compile(source);
-    var html = template(context)
-    $('#requests-list').append(html)
-  })
-  $('body').on('click','.create-mass-deal', function(e){
-    e.preventDefault();
-    $('#create-form-container').modal({overlayClose: true});
-    $.ajax({
-      url: $(this).attr('href'),
-    }).done(function(data){
-        // $.modal.close();
-      })
-  })
-
   $("#requests-list").on('click','.make_an_offer_link', function(event){
     event.preventDefault();
     var customerDealID=$(this).attr('href')
@@ -40,37 +19,32 @@ $(document).ready(function(){
     });
 
   });
-// })
-}); //close document
 
-$("body").on('submit','.create_mass_deal_form', function(e){
-e.preventDefault();
-$.ajax({
-  url:$(this).attr("action"),
-  data:$('#create-form').serialize(),
-  type:'POST'
-}).done(function(data){
-  console.log(data)
-  var html = "<li id=mass"+data.id+" class='deal deal-"+data.id+"'><img src='"+data.deal_image+"'><h3> "+data.name+" </h3><p>"+ data.short_description +"</p></li>"
-$(".mass-deals-list").append(html)
- $.modal.close();
-})
+  $("body").on('submit','.create_mass_deal_form', function(e){
+    e.preventDefault();
+    $.ajax({
+      url:$(this).attr("action"),
+      data:$('#create-form').serialize(),
+      type:'POST'
+    }).done(function(data){
+      console.log(data)
+      var html = "<li id=mass"+data.id+" class='deal deal-"+data.id+"'><img src='"+data.deal_image+"'><h3> "+data.name+" </h3><p>"+ data.short_description +"</p></li>"
+      $(".mass-deals-list").append(html)
+      $.modal.close();
+    })
 
-})
+  })
 
 
-$('body').on('click','.cancel_mass_deal_link', function(e){
-  e.preventDefault()
-  $.ajax({
-  url:$(this).attr("href"),
-  type:'DELETE'
-}).done(function(data){
-  console.log(data)
-  // $('#'+data.id).remove();
-})
-})
-
-
+  $('body').on('click','.cancel_mass_deal_link', function(e){
+    e.preventDefault()
+    $.ajax({
+      url:$(this).attr("href"),
+      type:'DELETE'
+    }).done(function(data){
+      console.log(data)
+    })
+  })
 
 });
 
