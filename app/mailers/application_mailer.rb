a17fd32dadbcec1030dcf074d05e1bed3d1dce62
Curtis Seaton <infin80design@gmail.com>
2015-04-21 10:10:10 -0700

    Creates welcome email upon customer signup

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "no-reply@dinnerwinner.io"
   layout 'mailer'
 end
--- app/mailers/customer_mailer.rb
+++ app/mailers/customer_mailer.rb
@@ -1,6 +1,6 @@
 class CustomerMailer < ApplicationMailer
 	def welcome_email(customer)
     @customer = customer
-    mail(to: @customer.email, subject: "Welcome to Curtis.com")
+    mail(to: @customer.email, subject: "Welcome to Dinner Winner")
   end
 end
