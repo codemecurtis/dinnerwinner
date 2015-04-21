51ec5ca35f52654f4b9ac3e9a436df0e963fc361
Curtis Seaton <infin80design@gmail.com>
2015-04-21 16:10:17 -0700

    Creates new deal offer email

--- app/mailers/customer_mailer.rb
+++ app/mailers/customer_mailer.rb
@@ -3,4 +3,11 @@ class CustomerMailer < ApplicationMailer
     @customer = customer
     mail(to: @customer.email, subject: "Welcome to Dinner Winner")
   end
+
+  def deal_accept(deal, customer, business)
+  	@customer_deal = deal
+  	@customer = customer
+  	@business = business
+    mail(to: @customer.email, subject: "You have a new deal from #{@business.name}")
+  end
 end
