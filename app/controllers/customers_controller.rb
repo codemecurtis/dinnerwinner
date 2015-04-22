98750f6c8ec52a3e642007370909606b13b809f5
Stephanie Lopez <stephanielopez.sf@gmail.com>
2015-04-22 12:26:24 -0700

    change accepted deals DOM format, add more info about the deal

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -14,6 +14,7 @@ class CustomersController < ApplicationController
     @all_customer_deals.each do |deal|
       if deal.accepted == false && deal.deal_id != nil
         @pending_deals << deal
+        @business_name = Business.where(id: deal.deal.business_id).first.name
       elsif deal.deal_id != nil
         @accepted_deals << deal
       end
