6b9f47080714963f827664722683425fd011533a
Curtis Seaton <infin80design@gmail.com>
2015-04-22 00:06:43 -0700

    Adds JS callbacks to customer login and signup modal windows

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -3,15 +3,43 @@ $(document).on('page:change', function(){
   $('.customer-signup-link').on('click', function(e){
       e.preventDefault();
       $('.signup-modal').modal({
-        overlayClose: true
-      })
+      overlayClose: true,
+      onOpen: function (dialog) {
+        dialog.overlay.fadeIn('slow', function () {
+          dialog.container.slideDown('slow', function () {
+            dialog.data.fadeIn('slow');
+          });
+        });
+      },
+      onClose: function (dialog) {
+        dialog.container.slideUp('slow', function () {
+          dialog.overlay.fadeOut('slow', function () {
+            dialog.data.fadeIn('slow');
+          });
+        });
+      }
+    });
     })
 
   $('.customer-login-link').on('click', function(e){
     e.preventDefault();
     $('.login-modal').modal({
-      overlayClose: true
-    })
+      overlayClose: true,
+      onOpen: function (dialog) {
+        dialog.overlay.fadeIn('slow', function () {
+          dialog.container.slideDown('slow', function () {
+            dialog.data.fadeIn('slow');
+          });
+        });
+      },
+      onClose: function (dialog) {
+        dialog.container.slideUp('slow', function () {
+          dialog.overlay.fadeOut('slow', function () {
+            dialog.data.fadeIn('slow');
+          });
+        });
+      }
+    });
   })
 
 });
